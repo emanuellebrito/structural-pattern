149118a8001d914814c38b1bb0e018419d9ebbd4
Emanuelle Brito <emanuelle.biologa@gmail.com>
2023-05-26 22:58:43 -0300

    add metrics

--- 1_matrices_function.R
+++ 1_matrices_function.R
@@ -1,77 +1,48 @@
-########## função para automatizar cálculos de matrizes de interações #######
-
-LIEB.NETWORK <- function(diretorio, ext, aleats = 999)#(diretorio de arquivos, ext(extencao usada. ex= txt ou csv))
-{
+LIEB.NETWORK <- function(diretorio, ext, aleats = 999) {
+  library(bipartite)  # Load bipartite package
   setwd(diretorio)
-  dados <- list.files(diretorio)
+  dados <- list.files(diretorio, pattern = ext, full.names = TRUE)
   n <- length(dados)
-  matriz <- list()
-  for (i in 1:n)
-  {
-    matriz[[i]] <- read.csv(dados[i], h=T, sep = ";", dec = ",")
-  }
+  matriz <- vector("list", n)
   
-  C <- numeric(n)
-  for (i in 1:n)
-  {
-    ip <- nrow(matriz[[i]])*ncol(matriz[[i]])
-    iobs1 <- ifelse(matriz[[i]] > 0,1,0)
-    iobs <- sum(iobs1)
-    resuC <- ip/iobs
-    C[i] <- resuC
-  }
-  
-  p.C <- matrix( ,ncol = aleats, nrow = n)
-  for (i in 1:n)
-  {
-    mattemp <- matriz[[i]]
-    
-    for (k in 1:aleats)
-    {
-      aleat <- matrix(sample(rep(c(0,1),ncol(mattemp)*nrow(mattemp)), replace=T), nrow=nrow(mattemp), ncol=ncol(mattemp))
-      ip <- nrow(aleat)*ncol(aleat)
-      iobs <- sum(aleat)
-      resuCR <- ip/iobs
-      p.C[i,k] <- resuCR
-    }
-  }
-  
-  C.abs <- abs(C)
-  p.C.abs <- abs(p.C)
-  p <- numeric(n)
-  
-  for (i in 1:n)
-  {
-    extremos <- sum(p.C.abs[i, ] >= C.abs[i])
-    p[i] <- (extremos+1) / (aleats+1)
-  }
-  
-  AI <- numeric(n)
-  for (i in 1:n)
-  {
-    num.dif <- ncol(matriz[[i]]) - nrow(matriz[[i]])
-    den.somas <- sum(dim(matriz[[i]]))
-    resuAI <- num.dif/den.somas
-    AI[i] <- resuAI
+  for (i in 1:n) {
+    matriz[[i]] <- read.csv(dados[i], header = TRUE, sep = ";", dec = ",")
   }
   
   size <- numeric(n)
-  for (i in 1:n)
-  {
+  for (i in 1:n) {
     num.net <- ncol(matriz[[i]]) + nrow(matriz[[i]])
     size[i] <- num.net
   }
   
-  ResuMat <- matrix( , nrow = n, ncol = 4)
-  rownames(ResuMat) <- dados
-  colnames(ResuMat) <- c("C", "p", "AI", "size")
-  ResuMat[ , 1] <- C
-  ResuMat[ , 2] <- p
-  ResuMat[ , 3] <- AI
-  ResuMat[ , 4] <- size
+  C <- numeric(n)
+  AS <- numeric(n)
+  NODF <- numeric(n)
+  
+  for (i in 1:n) {
+    binaryMat <- ifelse(matriz[[i]] > 0, 1, 0)  # Convert to binary incidence matrix
+    
+    connectance <- networklevel(binaryMat, index = "connectance")  # Connectance from bipartite
+    C[i] <- round(connectance, 3)
+    
+    asymmetry <- networklevel(binaryMat, index = "web asymmetry")  # Asymmetry from bipartite
+    AS[i] <- round(asymmetry, 3)
+    
+    nestedness <- networklevel(binaryMat, index = "NODF")  # NODF from bipartite
+    NODF[i] <- round(nestedness, 3)
+  }
+  
+  ResuMat <- matrix(nrow = n, ncol = 4)  
+  rownames(ResuMat) <- basename(dados)
+  colnames(ResuMat) <- c("size", "C", "AS", "NODF") 
+  ResuMat[, 1] <- size
+  ResuMat[, 2] <- C
+  ResuMat[, 3] <- AS
+  ResuMat[, 4] <- NODF  
   
   return(ResuMat)
 }
+
 
 LIEB.NETWORK(diretorio = "C:/Users/emanu/Dropbox (Personal)/Doutorado - Emanuelle/Cap 1 - Scientometric/data/pollination_webs" , ext = ".csv")
 
