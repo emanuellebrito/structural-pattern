0c24ed77c5a92d8a076ecc0e003e25bab2651329
Emanuelle Brito <emanuelle.biologa@gmail.com>
2023-06-03 15:10:22 -0300

    add new function

--- 1_matrices_function.R
+++ 1_matrices_function.R
@@ -18,7 +18,7 @@ LIEB.NETWORK <- function(diretorio, ext) {
   setwd(diretorio)
   dados <- list.files(diretorio, pattern = ext, full.names = TRUE)
   n <- length(dados)
-  matrix <- vector("list", n)
+  web <- vector("list", n)
   
   size <- numeric(n)
   C <- numeric(n)
@@ -28,11 +28,11 @@ LIEB.NETWORK <- function(diretorio, ext) {
   Animals <- numeric(n)
   
   for (i in 1:n) {
-    matrix[[i]] <- read.csv(dados[i], header = TRUE, sep = ";", dec = ",")
+    web[[i]] <- read.csv(dados[i], header = TRUE, sep = ";", dec = ",")
     
     # Calculate network properties
-    binaryMat <- ifelse(matrix[[i]] > 0, 1, 0)
-    num.net <- ncol(matrix[[i]]) + nrow(matrix[[i]])
+    binaryMat <- ifelse(web[[i]] > 0, 1, 0)
+    num.net <- ncol(web[[i]]) + nrow(web[[i]])
     size[i] <- num.net - 1
     
     connectance <- networklevel(binaryMat, index = "connectance")
@@ -45,13 +45,13 @@ LIEB.NETWORK <- function(diretorio, ext) {
     NODF[i] <- round(nestedness, 3)
     
     # Calculate species richness
-    richness_p <- ncol(matrix[[i]])
-    richness_a <- nrow(matrix[[i]])
+    richness_p <- ncol(web[[i]])
+    richness_a <- nrow(web[[i]])
     Plants[i] <- richness_p - 1
     Animals[i] <- richness_a
   }
   
-  ResuMat <- matrix(nrow = n, ncol = 6)  
+  ResuMat <- web(nrow = n, ncol = 6)  
   rownames(ResuMat) <- basename(dados)
   colnames(ResuMat) <- c("size", "C", "AS", "NODF", "Plants", "Animals")
   ResuMat[, 1] <- size
@@ -66,3 +66,95 @@ LIEB.NETWORK <- function(diretorio, ext) {
 
 cap3_richness <- LIEB.NETWORK(diretorio = "C:/Users/emanu/Dropbox (Personal)/Doutorado - Emanuelle/Cap 1 - Scientometric/data/pollination_webs" , ext = ".csv")
 write.csv(cap3_richness, "results_networks.csv", row.names=T)
+
+
+#Since modularity takes a long time to process its better do it apart
+
+LIEB.NETWORK <- function(diretorio, ext) {
+  library(bipartite)  # Load bipartite package
+  setwd(diretorio)
+  dados <- list.files(diretorio, pattern = ext, full.names = TRUE)
+  n <- length(dados)
+  matriz <- vector("list", n)
+  
+  for (i in 1:n) {
+    matriz[[i]] <- read.csv(dados[i], header = TRUE, sep = ";", dec = ",")
+    matriz[[i]] <- matriz[[i]][, -1]  # Remove the first column
+    matriz[[i]] <- as.matrix(matriz[[i]])
+  }
+  
+  modularity <- numeric(n)
+  
+  for (i in 1:n) {
+    print(paste("Processing matrix:", i))
+    print(paste("Class:", class(matriz[[i]])))
+    print(paste("Dimensions:", dim(matriz[[i]])))
+    QuanBiMo <- computeModules(matriz[[i]], method = "Beckett", steps = 10)
+    modularity[i] <- round(QuanBiMo@likelihood, 3)
+    print(paste("Modularity value:", modularity[i]))
+  }
+  
+  compartments <- numeric(n)
+  
+  for (i in 1:n) {
+    comp <- networklevel(matriz[[i]], index = "number of compartments")
+    compartments[i] <- comp
+  }
+  
+  ResuMat <- matrix(nrow = n, ncol = 2)  
+  rownames(ResuMat) <- basename(dados)
+  colnames(ResuMat) <- c("modularity", "compartments")
+  ResuMat[, 1] <- modularity
+  ResuMat[, 2] <- compartments
+  
+  return(ResuMat)
+}
+
+
+#Improved function to mapping possible errors reading the matrices
+
+LIEB.NETWORK <- function(diretorio, ext) {
+  library(bipartite)  # Load bipartite package
+  setwd(diretorio)
+  dados <- list.files(diretorio, pattern = ext, full.names = TRUE)
+  n <- length(dados)
+  matriz <- vector("list", n)
+  
+  results <- list(modularity = numeric(), compartments = numeric()) #matrix_name = character())
+  
+  for (i in 1:n) {
+    print(paste("Processing matrix:", i))
+    
+    tryCatch({
+      matriz[[i]] <- read.csv(dados[i], header = TRUE, sep = ";", dec = ",")
+      matriz[[i]] <- matriz[[i]][, -1]  # Remove the first column
+      matriz[[i]] <- as.matrix(matriz[[i]])
+      
+      print(paste("Class:", class(matriz[[i]])))
+      print(paste("Dimensions:", dim(matriz[[i]])))
+      
+      QuanBiMo <- computeModules(matriz[[i]], method = "Beckett", steps = 10)
+      modularity <- round(QuanBiMo@likelihood, 3)
+      print(paste("Modularity value:", modularity))
+      
+      comp <- networklevel(matriz[[i]], index = "number of compartments")
+      compartments <- comp
+      
+      # Store the processed results in the 'results' list
+      results$matrix_name <- c(results$matrix_name, basename(dados[i]))
+      results$modularity <- c(results$modularity, modularity)
+      results$compartments <- c(results$compartments, compartments)
+    }, error = function(e) {
+      warning_message <- paste("Error processing matrix:", i)
+      warning(warning_message)
+    })
+  }
+  
+  # Save the 'results' list in a separate object
+  save(results, file = "processed_results.RData")
+}
+
+
+
+
+subset1 <- LIEB.NETWORK(diretorio = "C:/Users/emanu/Dropbox (Personal)/Doutorado - Emanuelle/Cap 1 - Scientometric/data/pollination_webs" , ext = ".csv")
