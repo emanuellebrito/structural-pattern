89976235d23ad2d91097d3dc9ff341cb5bb8c11e
Emanuelle Brito <emanuelle.biologa@gmail.com>
2023-06-10 11:38:56 -0300

    improving plots

--- 5_data_inspection.R
+++ 5_data_inspection.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(ggplot2)
+library(patchwork)
+library(cowplot)
+library(ggplotify)
 
 #Read data
 data <- read.csv("results_networks.csv", header = TRUE, sep = ";", dec = ",")
@@ -15,16 +18,29 @@ data_mean <- data.frame(Region = data$Region, Hemisphere = data$Hemisphere,
 
 #Checking the means and sd for each variable levels
 count_df <- data_mean %>%
-  select(Region, Hemisphere, Continent, Biome, Realm, Connectance) %>%
-  gather(key = "Variable", value = "Value", -Connectance) %>%
+  select(Region, Hemisphere, Continent, Biome, Realm, Connectance, NODF, M_Beckett) %>%
+  gather(key = "Variable", value = "Value", -Connectance,-NODF,-M_Beckett) %>%
   group_by(Variable, Value) %>%
   summarise(Count = n(), Connectance_mean = mean(Connectance), 
             Connectance_sd = sd(Connectance), NODF_mean = mean(NODF), 
             NODF_sd = sd(NODF), M_Beckett_mean = mean(M_Beckett), 
             M_Beckett_sd = sd(M_Beckett))
 
+# Create a new column "range" with categorized values for "Size"
+data_group <- data_mean %>%
+  mutate(range = cut(Size, breaks = c(0, 25, 50, 100, Inf),
+                     labels = c("0-25", "26-50", "51-100", ">100"), include.lowest = TRUE))
 
-count_df <- as.data.frame(count_df)
+# Count the frequencies in each range for "Size" and the count of different taxonomic levels
+# Here I classified networks into mini, small, medium and large
+count_data_combined <- data_group %>%
+  group_by(range) %>%
+  summarize(n = n(), mean_NODF = mean(NODF), sd_NODF = sd(NODF),
+            mean_mod = mean(M_Beckett), sd_mod = sd(M_Beckett),
+            mean_connect = mean(Connectance), sd_connect = sd(Connectance))
+
+# Check data
+print(count_data_combined)
 
 #Reorder levels according to variables group
 levels_order <- c("Deserts and xeric shrublands", "Flooded grasslands and savannas", 
@@ -38,91 +54,48 @@ levels_order <- c("Deserts and xeric shrublands", "Flooded grasslands and savann
 
 
 ###########################################
-
 # Create a new data frame with: Variables, Levels and Metrics
 new_data <- data %>%
   select(Hemisphere, Region, Continent, Biome_WWF, Realm_WWF, Connectance, NODF, M_Beckett) %>%
   pivot_longer(cols = c(-Connectance, -NODF, -M_Beckett), names_to = "Variable", values_to = "Value")
 
-# View the new data set
+# Check the data
 print(new_data)
 
 #Plotting the means for each level of variables groups
-#Connectance
-new_data %>%
-  mutate(Value = factor(Value, levels = rev(levels_order))) %>%
-  ggplot(aes(x=Value,y=Connectance)) +
-  geom_boxplot(width=0.25, fill = "white", outlier.color = "black")+
-  geom_rect(aes(xmin = 0, xmax = 2.5, ymin = -Inf, ymax = +Inf), alpha = 0.01, fill = "#70D6FF") +
-  geom_rect(aes(xmin = 2.5, xmax = 9.5, ymin = -Inf, ymax = +Inf), alpha = 0.01, fill = "#FF70A6") +
-  geom_rect(aes(xmin = 9.5, xmax = 11.5, ymin = -Inf, ymax = +Inf), alpha = 0.01, fill = "#FF9770") +
-  geom_rect(aes(xmin = 11.5, xmax = 17.5, ymin = -Inf, ymax = +Inf), alpha = 0.01, fill = "#FFD670") +
-  geom_rect(aes(xmin = 17.5, xmax = 30.5, ymin = -Inf, ymax = +Inf), alpha = 0.01, fill = "#E9FF70") +
-  xlab("Geographic reference") + 
-  ylab("Connectance") +
-  coord_flip()
-
-new_data %>%
-  mutate(Value = factor(Value, levels = rev(levels_order))) %>%
-  ggplot(aes(x=Value,y=Connectance, color=Variable)) +
-  geom_boxplot(width=0.25)+
-  xlab("Geographic reference") + 
-  ylab("Connectance") +
-  coord_flip()
-
-new_data %>%
-  mutate(Value = factor(Value, levels = rev(levels_order))) %>%
-  ggplot(aes(x=Value,y=NODF, color=Variable)) +
-  geom_boxplot(width=0.25)+
-  xlab("Geographic reference") + 
-  ylab("Nestedness") +
-  coord_flip()
-
-new_data %>%
-  mutate(Value = factor(Value, levels = rev(levels_order))) %>%
-  ggplot(aes(x=Value,y=M_Beckett, color=Variable)) +
-  geom_boxplot(width=0.25)+
-  xlab("Geographic reference") + 
-  ylab("Modularity") +
-  coord_flip()
-
-
-
-#####
-library(patchwork)
-library(cowplot)
-library(ggplotify)
-
 plot1 <- new_data %>%
   mutate(Value = factor(Value, levels = rev(levels_order))) %>%
   ggplot(aes(x = Value, y = Connectance, color = Variable)) +
-  geom_boxplot(width = 0.25) +
+  geom_boxplot(width = 0.35) +
   xlab("Geographic reference") + 
   ylab("Connectance") +
   coord_flip() +
-  theme(legend.position="none")
+  theme_minimal()+
+  theme(legend.position = c(-0.9, 0.1), axis.text.y = element_text(size = 16),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 16))
 
 plot2 <- new_data %>%
   mutate(Value = factor(Value, levels = rev(levels_order))) %>%
   ggplot(aes(x = Value, y = NODF, color = Variable)) +
-  geom_boxplot(width = 0.25) +
+  geom_boxplot(width = 0.35) +
   xlab("") + 
   ylab("Nestedness") +
   coord_flip() +
-  theme(legend.position="none") 
+  theme_minimal()+
+  theme(legend.position="none",
+        axis.title.x = element_text(size = 16)) 
 
 plot3 <- new_data %>%
   mutate(Value = factor(Value, levels = rev(levels_order))) %>%
   ggplot(aes(x = Value, y = M_Beckett, color = Variable)) +
-  geom_boxplot(width = 0.25) +
+  geom_boxplot(width = 0.35) +
   xlab("") + 
   ylab("Modularity") +
   coord_flip() +
-  theme(legend.position="none")
-
-# Arrange the plots side by side
-combined_plot <- plot1 + plot2 + plot3 +
-  plot_layout(ncol = 3, guides = "collect")
+  theme_minimal()+
+  theme(legend.position="none",
+        axis.title.x = element_text(size = 16))
 
 # Convert each plot to a gtable object
 plot1_gtable <- as_gtable(ggplotGrob(plot1))
@@ -150,8 +123,59 @@ combined_plot <- as.ggplot(combined_gtable)
 # Display the combined plot
 combined_plot
 
-
-tiff('geo_metrics.tif', w=2700, h=4000, units="px", res=300, compression = "lzw")
+# Saving figure
+tiff('geo_metrics.tif', w=4700, h=4800, units="px", res=300, compression = "lzw")
 combined_plot
 dev.off()
+
+############################
+# Checking data for taxonomic variables
+# First remove the NA's
+data <- drop_na(data) 
+
+# Create a new dataset only with taxnomic variables
+taxon <- data.frame(Plant_level = data$Plant_taxonomic_level,
+                        Plant_group = data$Plant_taxon_group, 
+                        Animal_level = data$Animal_taxonomic_level,
+                        Animal_group = data$Animal_taxon_group, 
+                        Connectance = data$Connectance,
+                        NODF = data$NODF, M_Beckett = data$M_Beckett, Size = data$Size,
+                        Asymmetry = data$Asymmetry)
+
+#Checking the means and sd for each variable levels
+count_taxon <- taxon %>%
+  select(Plant_level, Plant_group, Animal_level, Animal_group, Connectance, NODF, M_Beckett) %>%
+  gather(key = "Variable", value = "Value", -Connectance, -NODF, -M_Beckett) %>%
+  group_by(Variable, Value) %>%
+  summarise(Count = n(), Connectance_mean = mean(Connectance), 
+            Connectance_sd = sd(Connectance), NODF_mean = mean(NODF), 
+            NODF_sd = sd(NODF), M_Beckett_mean = mean(M_Beckett), 
+            M_Beckett_sd = sd(M_Beckett))
+
+
+
+# Plotting
+plot1 <- taxon %>%
+  ggplot(aes(x = Animal_level, y = Connectance)) +
+  geom_boxplot(width = 0.35) +
+  xlab("Taxonomic level") + 
+  ylab("Connectance") +
+  coord_flip() +
+  theme_minimal()+
+  theme(legend.position = c(-0.9, 0.1), axis.text.y = element_text(size = 16),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 16))
+
+# Plotting
+plot2 <- taxon %>%
+  ggplot(aes(x = Animal_group, y = Connectance)) +
+  geom_boxplot(width = 0.35) +
+  xlab("Taxonomic group") + 
+  ylab("Connectance") +
+  coord_flip() +
+  theme_minimal()+
+  theme(legend.position = c(-0.9, 0.1), axis.text.y = element_text(size = 16),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 16))
+
 
